98c6a8c760afdf49135a763a9b50d8b77309baed
yechoi42 <yechoi@student.42seoul.kr>
2022-02-10 23:36:18 +0900

    feat: add global settings

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import NavBar from '../components/NavBar'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <div>
+    <NavBar />
+    <Component {...pageProps} />
+    <style jsx global>{`
+      a {
+        color: red;
+      }
+    `}</style>
+    </div>
 }
 
 export default MyApp
